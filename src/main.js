dce1e40aa83674b3e6c16514e35f824133bff413
ArtemMartynow <artem1435@i.ua>
2023-04-28 02:08:26 +0300

    connected the fonts

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import globalComponents from './components/global/index.js' 
 
+import './assets/fonts.css'
 
 createApp(App).mount('#app')
 const app = createApp(App)
