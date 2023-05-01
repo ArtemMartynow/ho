3aa433ce32dd1a57c0eccc2ca07ed8db9c194392
ArtemMartynow <artem1435@i.ua>
2023-05-02 00:54:27 +0300

    add style for HomePage

--- src/components/global/HomePage.vue
+++ src/components/global/HomePage.vue
@@ -5,12 +5,12 @@
 <template>
     <div class="home-page">
         <div class="home-page-content">
-            <h1>Welcome to Yoga Ho' ponopono</h1>
+            <h1>Welcome <br>to Yoga Ho' ponopono</h1>
             <p>A caress to the soul, do yoga.</p>
         </div>    
         <div class="home-page-img">
-            <img src="../../assets/images/Vector-Scotch.png">
-            <img src="../../assets/images/Place-Your-Image-Her.png" id="lantern">
+            <img src="../../assets/images/Vector-Scotch.png" id="lantern">
+            <img src="../../assets/images/Place-Your-Image-Her.png">
         </div>
     </div>
 </template>
