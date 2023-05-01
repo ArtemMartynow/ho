c037d31341533858d5399f7b0fb1dd9e3007b7f3
ArtemMartynow <artem1435@i.ua>
2023-05-01 20:17:54 +0300

    corrected header

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -3,16 +3,18 @@
 </script>
 
 <template>
-    <div class="links">
-        <a href="#">LESSONS</a>
-        <span>∙</span>
-        <a href="#">ABOUT</a>
-        <span>∙</span>
-        <a href="#">GIFT CARD</a>
-        <span>∙</span>
-        <a href="#">CONTACT</a>
-    </div>
-    <div class="logo">
-        <img src="../../assets/images/Place-Your-Logo-Here.png">
-    </div>
+    <header>
+        <div class="links">
+            <a href="#">LESSONS</a>
+            <span>∙</span>
+            <a href="#">ABOUT</a>
+            <span>∙</span>
+            <a href="#">GIFT CARD</a>
+            <span>∙</span>
+            <a href="#">CONTACT</a>
+        </div>
+        <div class="logo">
+            <img src="../../assets/images/Place-Your-Logo-Here.png">
+        </div>
+    </header>
 </template>
