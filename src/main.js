b40d30b435395c6da0f43501bef69e62865fba8e
ArtemMartynow <artem1435@i.ua>
2023-05-02 00:12:09 +0300

    conected style HomePage and text

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import globalComponents from './components/global/index.js' 
 
 import './assets/fonts.css'
+import './assets/text.css'
 import './assets/header.css'
+import './assets/home-page.css'
 
 const app = createApp(App)
 app.use(globalComponents)
