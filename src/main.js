12655de42698f6da1d874ba94d1964832a563308
ArtemMartynow <artem1435@i.ua>
2023-05-02 01:40:05 +0300

    conected style for about and btns

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import globalComponents from './components/global/index.js'
 
 import './assets/fonts.css'
 import './assets/text.css'
+import './assets/btns.css'
 import './assets/header.css'
 import './assets/home-page.css'
+import './assets/about.css'
 
 const app = createApp(App)
 app.use(globalComponents)
