cc064d907e7e2442d538b421dac052844acac85b
ArtemMartynow <artem1435@i.ua>
2023-05-03 01:41:19 +0300

    create markings for AteLierBorn

--- src/components/global/AteLierBorn.vue
+++ src/components/global/AteLierBorn.vue
@@ -3,5 +3,24 @@
 </script>
 
 <template>
-    
+    <div class="ate-lier-born">
+        <div class="ate-lier-born-contend">
+            <div class="ate-lier-born-img">
+                <img src="../../assets/images/Vector-Scotch(2).png" id="right-scotch">
+                <img src="../../assets/images/Vector-Scotch(1).png" id="left-scotch">
+                <img src="../../assets/images/img.png">
+                <img src="../../assets/images/Vector-Stamp.png" id="stamp">
+                <p>
+                    Lorem ipsum dolor sit amet, nihil 
+                    nominavi delicatissimi et pri, harum 
+                    apeirian sea te. Per quod prima tamquam 
+                    cu, id case rebum simul vim. Lorem ipsum 
+                    dolor sit amet, lorem ipsum dolor sit 
+                    amet, lorem ipsum.
+                </p>
+            </div>
+            <h2>Ate lier Born</h2>
+        </div>
+        <h3>A caress to the soul, do yoga.</h3>
+    </div>
 </template>
