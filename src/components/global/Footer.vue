8d8b256a4a1ffcfe892efc26165076d5e982deeb
ArtemMartynow <artem1435@i.ua>
2023-05-04 02:50:30 +0300

    create markings Footer

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -3,5 +3,16 @@
 </script>
 
 <template>
-    
+    <footer>
+        <div class="footer-top">
+            <p>visit us: yoga 13th street ny.</p>
+            <p>follow us: @do_yoga_ho</p>
+            <p>atelier open: 9am - 9pm</p>
+        </div>
+        <div class="footer-bottom">
+            <a href="#">enter your email here and subscribe</a>
+            <img src="../../assets/images/Place-Your-Logo-Here.png">
+            <p></p>
+        </div>
+    </footer>
 </template>
