42993180a07e418e4e9ec34c2c9521b89c398193
ArtemMartynow <artem1435@i.ua>
2023-05-04 02:29:55 +0300

    add style for RelaxTime

--- src/components/global/RelaxTime.vue
+++ src/components/global/RelaxTime.vue
@@ -6,11 +6,11 @@
     <div class="relax-time">
         <h2>Relax Time</h2>
         <p>
-            Do you want to give away our services? We have a 
+            Do you want to give away our services?<br> We have a 
             gift card for you.
         </p>
         <button class="relax-time-btn">get it here</button>
-        <img src="../../assets/images/Vector-Stamp.png" id="left-stamp">
-        <img src="../../assets/images/Vector-Stamp.png" id="right-stamp">
+        <img src="../../assets/images/Vector-Stamp(1).png" id="left-stamp">
+        <img src="../../assets/images/Vector-Stamp(1).png" id="right-stamp">
     </div>
 </template>
