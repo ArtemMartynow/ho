86fb6d5bf75ca7380686f92966a9ef87730c6b97
ArtemMartynow <artem1435@i.ua>
2023-05-04 03:39:22 +0300

    create markings Message

--- src/components/global/Message.vue
+++ src/components/global/Message.vue
@@ -3,5 +3,13 @@
 </script>
 
 <template>
-    
+    <div class="message">
+        <h2>Hi! We await your message</h2>
+        <form @submit.prevent.stop="submit">
+            <input type="text">
+            <input type="email">
+            <textarea cols="30" rows="10"></textarea>
+            <button class="form-btn">send</button>
+        </form>
+    </div>
 </template>
