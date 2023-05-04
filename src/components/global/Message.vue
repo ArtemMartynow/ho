cbc6ab1f14b0cb1d06df4b0e5925b762ac23fa6c
ArtemMartynow <artem1435@i.ua>
2023-05-04 04:09:23 +0300

    add style for Message

--- src/components/global/Message.vue
+++ src/components/global/Message.vue
@@ -7,12 +7,8 @@
         <h2>Hi! We await your message</h2>
         <form @submit.prevent.stop="submit">
             <input type="text" placeholder="NAME SURNAME">
-            <input type="email" placeholder="E-MAIL">
-            <textarea 
-                cols="30" 
-                rows="10" 
-                placeholder="|MESSAGE"
-            ></textarea>
+            <input type="email" placeholder="E-MAIL" id="email">
+            <textarea  placeholder="| MESSAGE"></textarea>
             <button class="form-btn">send</button>
         </form>
     </div>
