37a9e9251ba3f96407e15611cd42059a109d03d0
ArtemMartynow <artem1435@i.ua>
2023-05-04 03:42:55 +0300

    corrected html

--- src/components/global/Message.vue
+++ src/components/global/Message.vue
@@ -6,9 +6,13 @@
     <div class="message">
         <h2>Hi! We await your message</h2>
         <form @submit.prevent.stop="submit">
-            <input type="text">
-            <input type="email">
-            <textarea cols="30" rows="10"></textarea>
+            <input type="text" placeholder="NAME SURNAME">
+            <input type="email" placeholder="E-MAIL">
+            <textarea 
+                cols="30" 
+                rows="10" 
+                placeholder="|MESSAGE"
+            ></textarea>
             <button class="form-btn">send</button>
         </form>
     </div>
